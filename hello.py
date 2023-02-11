8af737a946f5b78062fabfacd97ee241baf58c8e
Polyakov Daniil <70587587+Polliakoff@users.noreply.github.com>
2023-02-11 16:05:30 +0300

    fix new app 2

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ class DBManager:
     def __init__(self, database='example', host="db", user="root", password_file=None):
         pf = open(password_file, 'r')
         self.connection = mysql.connector.connect(
-            user=user, 
+            user=user,
             password=pf.read(),
             host=host, # name of the mysql service as set in the docker compose file
             database=database,
@@ -15,13 +15,13 @@ class DBManager:
         )
         pf.close()
         self.cursor = self.connection.cursor()
-    
+
     def populate_db(self):
         self.cursor.execute('DROP TABLE IF EXISTS blog')
         self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
         self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
         self.connection.commit()
-    
+
     def query_titles(self):
         self.cursor.execute('SELECT title FROM blog')
         rec = []
