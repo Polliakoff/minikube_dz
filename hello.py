3e3bbbe73b8893dd47753b4677ebba8f354860ed
Polyakov Daniil <70587587+Polliakoff@users.noreply.github.com>
2023-02-11 15:45:47 +0300

    new app version

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask
 import mysql.connector
+from prometheus_flask_exporter import PrometheusMetrics
 
 
 class DBManager:
     def __init__(self, database='example', host="db", user="root", password_file=None):
         pf = open(password_file, 'r')
         self.connection = mysql.connector.connect(
-            user=user,
+            user=user, 
             password=pf.read(),
             host=host, # name of the mysql service as set in the docker compose file
             database=database,
@@ -15,13 +16,13 @@ class DBManager:
         )
         pf.close()
         self.cursor = self.connection.cursor()
-
+    
     def populate_db(self):
         self.cursor.execute('DROP TABLE IF EXISTS blog')
         self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
         self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
         self.connection.commit()
-
+    
     def query_titles(self):
         self.cursor.execute('SELECT title FROM blog')
         rec = []
@@ -32,12 +33,13 @@ class DBManager:
 
 server = Flask(__name__)
 conn = None
+metrics = PrometheusMetrics(server)
 
 @server.route('/')
 def listBlog():
     global conn
     if not conn:
-        conn = DBManager(password_file='/app/secrets/db-password')
+        conn = DBManager(password_file='/app/db-password')
         conn.populate_db()
     rec = conn.query_titles()
 
